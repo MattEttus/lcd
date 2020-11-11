c8f3919aea02f78190f6d94c391e8f2d5a419009
Matt Ettus <MattEttus@users.noreply.github.com>
2020-11-10 18:37:52 -0800

    Simple LCD app

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    LCD1IN8.DisString(
+    0,
+    0,
+    "I pressed A",
+    LCD1IN8.Get_Color(LCD_COLOR.RED)
+    )
+    LCD1IN8.LCD_DisplayWindows(
+    0,
+    0,
+    0,
+    0
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    LCD1IN8.DisString(
+    0,
+    0,
+    "I pressed B",
+    LCD1IN8.Get_Color(LCD_COLOR.RED)
+    )
+    LCD1IN8.LCD_Display()
+})
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_SetBL(10)
+LCD1IN8.LCD_Clear()
 basic.forever(function () {
 	
 })
